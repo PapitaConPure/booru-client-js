dff4146654b77d79bd0589c161f45c6224e22075
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-13 20:20:49 -0300

    Documentation and typing

--- src/services/booru-client.ts
+++ src/services/booru-client.ts
@@ -19,8 +19,6 @@ import { shuffleArray } from '../utils/misc';
 /**@description Representa una conexión a un sitio Booru.*/
 export class BooruClient {
 	static readonly API_URI = 'https://gelbooru.com/index.php';
-	static readonly API_POSTS_URL = 'https://gelbooru.com/index.php';
-	static readonly API_TAGS_URL = 'https://gelbooru.com/index.php?page=dapi&s=tag&q=index';
 
 	static readonly POSTS_API = BooruClient.#createBooruEndpoint({
 		//timeout: 10000,
@@ -42,8 +40,8 @@ export class BooruClient {
 	#tagStoreChain: TagStore[];
 	#tagFetchThreshold: number;
 	#manualTagCleanup: boolean;
-	#cleanupIntervalMs;
-	#lastCleanup;
+	#cleanupIntervalMs: number;
+	#lastCleanup: number;
 
 	/**
 	 * @description Creates a {@link BooruClient} with the specified `credentials` and various other `options`.
@@ -107,7 +105,7 @@ export class BooruClient {
 	}
 
 	/**
-	 * @description Verifica que el código de estado de una respuesta sea 200 y que los datos de Post sean válidos.
+	 * @description Asserts that the status code of a response is 200 and that the Post data is valid before returning it.
 	 * @throws {BooruFetchError}
 	 * @throws {BooruUnknownPostError}
 	 */
@@ -134,7 +132,7 @@ export class BooruClient {
 	}
 
 	/**
-	 * @description Verifica que el código de estado de una respuesta sea 200 y que los datos de Post sean válidos.
+	 * @description Asserts that the status code of a response is 200 and that the Tag data is valid before returning it.
 	 * @throws {BooruFetchError}
 	 * @throws {BooruUnknownTagError}
 	 */
@@ -170,9 +168,10 @@ export class BooruClient {
 	}
 
 	/**
-	 * @description Devuelve resultados de búsqueda en forma de {@linkcode Post}s.
-	 * @param tags Tags a buscar
-	 * @param searchOptions Opciones de búsqueda
+	 * @description Searches a {@link Booru} to retrieve {@linkcode Post}s, results can be transformed based on the `searchOptions` provided.
+	 * @param tags Tags to search
+	 * @param searchOptions Search options
+	 * @returns An array containing the posts found during the search.
 	 * @throws {ReferenceError}
 	 * @throws {TypeError}
 	 * @throws {BooruFetchError}
@@ -198,7 +197,8 @@ export class BooruClient {
 	}
 
 	/**
-	 * @description Obtiene y devuelve un {@linkcode Post} por ID.
+	 * @description Obtains a {@link Post} from a {@link Booru}, based on the supplied ID.
+	 * @returns The obtained post, or `undefined` if no post was found with that ID.
 	 * @throws {ReferenceError}
 	 * @throws {TypeError}
 	 * @throws {BooruFetchError}
@@ -218,12 +218,13 @@ export class BooruClient {
 	}
 
 	/**
-	 * @description Obtiene y devuelve un {@linkcode Post} por enlace.
+	 * @description Obtains a {@link Post} from a {@link Booru}'s URL.
+	 * @returns The obtained post, or `undefined` if no post exists on that URL.
 	 * @throws {ReferenceError}
 	 * @throws {TypeError}
 	 * @throws {BooruFetchError}
 	 */
-	async fetchPostByUrl(postUrl: string): Promise<Post | undefined> {
+	async fetchPostByUrl(postUrl: URL | string): Promise<Post | undefined> {
 		const { apiKey, userId } = this.#getCredentials();
 
 		if (typeof postUrl !== 'string') throw TypeError('Invalid Post URL');
@@ -243,7 +244,8 @@ export class BooruClient {
 	}
 
 	/**
-	 * @description Obtiene y devuelve las {@linkcode Tag}s del {@linkcode Post} indicado.
+	 * @description Retrieves the {@linkcode Tag}s associated with the given {@linkcode Post}.
+	 * @returns An array containing the tags that were retrieved from the post.
 	 * @throws {ReferenceError}
 	 * @throws {TypeError}
 	 * @throws {BooruFetchError}
@@ -251,37 +253,50 @@ export class BooruClient {
 	async fetchPostTags(post: Post): Promise<Tag[]> {
 		if (!Array.isArray(post?.tags)) throw ReferenceError('Invalid Post');
 
-		return this.fetchTagsByNames(...post.tags);
+		return this.fetchTagsByNames({ names: post.tags });
 	}
 
 	/**
-	 * @description Obtiene un {@linkcode Post} desde el enlace indicado y devuelve sus {@linkcode Tag}s.
+	 * @description Retrieves the {@linkcode Tag}s of a {@link Post} identified by its URL.
+	 * @returns An array containing the tags that were retrieved from the post.
 	 * @throws {ReferenceError}
 	 * @throws {TypeError}
 	 * @throws {BooruFetchError}
 	 */
 	async fetchPostTagsByUrl(postUrl: string): Promise<Tag[] | undefined> {
 		const post = await this.fetchPostByUrl(postUrl);
-		return post ? this.fetchTagsByNames(...post.tags) : undefined;
+		return post ? this.fetchTagsByNames({ names: post.tags }) : undefined;
 	}
 
 	/**
-	 * @description Obtiene el {@linkcode Post} con la ID indicada y devuelve sus {@linkcode Tag}s.
+	 * @description Retrieves the {@linkcode Tag}s of a {@link Post} identified by its ID.
+	 * @returns An array containing the tags that were retrieved from the post.
 	 * @throws {ReferenceError}
 	 * @throws {TypeError}
 	 * @throws {BooruFetchError}
 	 */
 	async fetchPostTagsById(postId: string | number): Promise<Tag[] | undefined> {
 		const post = await this.fetchPostById(postId);
-		return post ? this.fetchTagsByNames(...post.tags) : undefined;
+		return post ? this.fetchTagsByNames({ names: post.tags }) : undefined;
 	}
 
 	/**
+	 * @description Obtains {@link Tag}s by their names. Uses the configured {@link TagStore} chain as sequential cache layers before actually making an API call.
+	 *
+	 * Missing tags are fetched from the configured {@link Booru}'s API and stored back into the cache.
+	 * @returns An array containing every obtained tag.
 	 * @throws {ReferenceError}
 	 * @throws {TypeError}
 	 * @throws {BooruFetchError}
 	 */
-	async fetchTagsByNames(...tagNames: string[]): Promise<Tag[]> {
+	async fetchTagsByNames(options: {
+		/**The tag names to fetch.*/
+		names: string[];
+		/**Set to `true` if you want to skip all cache layers and fetch directly from the API (not recommended unless you have a reason to). `false` by default.*/
+		forceFetch?: boolean;
+	}): Promise<Tag[]> {
+		const { names: tagNames, forceFetch = false } = options;
+
 		this.#performAutoCleanup();
 
 		const { apiKey, userId } = this.#getCredentials();
@@ -290,10 +305,11 @@ export class BooruClient {
 
 		const normalizedTagNames = tagNames.map(decodeEntities);
 
-		const { storedTags, missingTagNames } =
-			normalizedTagNames.length < this.#tagFetchThreshold
-				? await this.#fetchTagNamesByTag(normalizedTagNames)
-				: await this.#fetchTagNamesByStore(normalizedTagNames);
+		const { storedTags, missingTagNames } = forceFetch
+			? { storedTags: [], missingTagNames: normalizedTagNames }
+			: normalizedTagNames.length < this.#tagFetchThreshold
+				? await this.#fetchTagsByNameFromStoreByName(normalizedTagNames)
+				: await this.#fetchTagsByNameFromStoreByStore(normalizedTagNames);
 
 		if (!missingTagNames.length) return storedTags;
 
@@ -322,24 +338,25 @@ export class BooruClient {
 	}
 
 	/**
-	 * @description Establece las credenciales a usar para cada búsqueda.
-	 * @param credentials Credenciales para autorizarse en la API
+	 * @description Sets the credentials used for all {@link Booru} API calls.
+	 * @param credentials Credentials for API authorization.
 	 * @throws {ReferenceError}
 	 * @throws {TypeError}
 	 */
-	setCredentials(credentials: Credentials) {
+	setCredentials(credentials: Credentials): this {
 		this.#expectCredentials(credentials);
 		this.#credentials = credentials;
 		return this;
 	}
 
 	/**
-	 * @description Performs a cleanup of all managed {@link TagStore}s so that invalid tags are re-fetched from the API or a store deeper in the chain.
+	 * @description Performs a cleanup of all managed {@link TagStore}s so that invalid or stale tags can be re-fetched from the API or a store deeper in the chain.
 	 *
-	 * Only call manually if this {@link BooruClient} was created using `manualTagCleanup:true`
+	 * Only call manually if this {@link BooruClient} was created using `manualTagCleanup: true`.
+	 * @param stores The stores to clean up.
 	 * @throws {InvalidOperationError}
 	 */
-	async performCleanup(...stores: TagStore[]) {
+	async performCleanup(...stores: TagStore[]): Promise<void> {
 		if (!this.#manualTagCleanup)
 			throw new InvalidOperationError(
 				`Manual cleanup cannot be called on a ${BooruClient.name} that has manualTagCleanup set to false`,
@@ -349,18 +366,18 @@ export class BooruClient {
 	}
 
 	/**
-	 * @description Recupera las credenciales de búsqueda establecidas.
+	 * @description Asserts that valid credentials are available for API requests and returns them.
 	 * @throws {ReferenceError}
 	 * @throws {TypeError}
 	 */
-	#getCredentials() {
+	#getCredentials(): Credentials {
 		this.#expectCredentials(this.#credentials);
 		return this.#credentials;
 	}
 
 	/**
-	 * @description Establece las credenciales a usar para cada búsqueda.
-	 * @param credentials Credenciales para autorizarse en la API
+	 * @description Validates credentials structure and ensures they are usable for API requests.
+	 * @param credentials Credentials to validate.
 	 * @throws {ReferenceError}
 	 * @throws {TypeError}
 	 */
@@ -372,7 +389,14 @@ export class BooruClient {
 			throw TypeError('User ID is invalid');
 	}
 
-	async #fetchTagNamesByTag(
+	/**
+	 * @description Obtains tags by tag names, using {@link TagStore} layers on a name by name basis.
+	 * @param normalizedTagNames The tag names from which to obtain tag objects.
+	 * @returns An object, containing:
+	 * * `storedTags`: Tags that could be obtained from a store layer.
+	 * * `missingTagNames`: Tag names that weren't available on any store.
+	 */
+	async #fetchTagsByNameFromStoreByName(
 		normalizedTagNames: string[],
 	): Promise<{ storedTags: Tag[]; missingTagNames: string[] }> {
 		const results = await Promise.all(
@@ -401,7 +425,14 @@ export class BooruClient {
 		};
 	}
 
-	async #fetchTagNamesByStore(
+	/**
+	 * @description Obtains tags by tag names, using {@link TagStore} layers on a store by store basis.
+	 * @param normalizedTagNames The tag names from which to obtain tag objects.
+	 * @returns An object, containing:
+	 * * `storedTags`: Tags that could be obtained from a store layer.
+	 * * `missingTagNames`: Tag names that weren't available on any store.
+	 */
+	async #fetchTagsByNameFromStoreByStore(
 		normalizedTagNames: string[],
 	): Promise<{ storedTags: Tag[]; missingTagNames: string[] }> {
 		const normalizedTagNamesToGet = new Set(normalizedTagNames);
@@ -426,6 +457,12 @@ export class BooruClient {
 		};
 	}
 
+	/**
+	 * @description Calls `cleanup()` on all the provided {@link TagStore}s, or the whole chain if no stores were specified.
+	 *
+	 * Managed by this {@link BooruClient} and throttled based on configuration during initialization.
+	 * @param stores The stores to clean up.
+	 */
 	async #performAutoCleanup(...stores: TagStore[]) {
 		if (this.#manualTagCleanup) return;
 
@@ -436,6 +473,10 @@ export class BooruClient {
 		await this.#performCleanup(...stores);
 	}
 
+	/**
+	 * @description Calls `cleanup()` on all the provided {@link TagStore}s, or the whole chain if no stores were specified.
+	 * @param stores The stores to clean up.
+	 */
 	async #performCleanup(...stores: TagStore[]) {
 		const targetStores = stores.length ? stores : this.#tagStoreChain;
 		const toCleanUp = targetStores.map((tagStore) => tagStore.cleanup?.());
