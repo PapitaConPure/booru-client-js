da7a4fcbee032d2b35e629feee0e8fab3d67f897
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-13 20:24:19 -0300

    Add Booru interface

---
+++ src/adapters/booru.ts
@@ -0,0 +1,35 @@
+import type { Post } from '../models/post';
+import type { Tag } from '../models/tag';
+import type { Credentials } from '../types/gelbooru';
+
+/**@description Defines una interface to interact with one of various imageboards.*/
+export default interface Booru {
+	/**
+	 *
+	 * @param tags
+	 * @param options
+	 * @param credentials
+	 */
+	search(tags: string, searchOptions: { limit: number, random: boolean }, credentials: Credentials): Promise<Post[]>;
+
+	/**
+	 *
+	 * @param postId
+	 * @param credentials
+	 */
+	fetchPostById(postId: string, credentials: Credentials): Promise<Post>;
+
+	/**
+	 *
+	 * @param postUrl
+	 * @param credentials
+	 */
+	fetchPostByUrl(postUrl: URL | string, credentials: Credentials): Promise<Post>;
+
+	/**
+	 *
+	 * @param names
+	 * @param credentials
+	 */
+	fetchTagsByNames(names: Iterable<string>, credentials: Credentials): Promise<Tag[]>;
+}
