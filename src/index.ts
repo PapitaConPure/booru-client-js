2435ba907f4832ca19785dbd705cdcdcd95459a1
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-13 13:14:07 -0300

    Exportar desde index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hello via Bun!')
+export * from './errors/booru';
+export * from './models/post';
+export * from './models/tag';
+export * from './services/booru';
+export * from './stores/memory-tag-store';
+export * from './stores/tag-store';
+export * from './types/gelbooru';
