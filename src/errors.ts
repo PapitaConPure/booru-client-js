3fecbf9b14e0df8101b135cf43aaaf78b6a0a012
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-13 11:00:15 -0300

    Use class.name instead of string literal for Error names

--- src/errors.ts
+++ src/errors.ts
@@ -1,27 +1,27 @@
 export class BooruError extends Error {
 	constructor(message: string, options?: ErrorOptions) {
 		super(message, options);
-		this.name = 'BooruError';
+		this.name = BooruError.name;
 	}
 }
 
 export class BooruFetchError extends BooruError {
 	constructor(message: string, options?: ErrorOptions) {
 		super(message, options);
-		this.name = 'BooruFetchError';
+		this.name = BooruFetchError.name;
 	}
 }
 
 export class BooruUnknownPostError extends BooruError {
 	constructor(message: string, options?: ErrorOptions) {
 		super(message, options);
-		this.name = 'BooruUnknownPostError';
+		this.name = BooruUnknownPostError.name;
 	}
 }
 
 export class BooruUnknownTagError extends BooruError {
 	constructor(message: string, options?: ErrorOptions) {
 		super(message, options);
-		this.name = 'BooruUnknownTagError';
+		this.name = BooruUnknownTagError.name;
 	}
 }
