5811f46aaeb311d4c66259a15f94655b8abdc9e3
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-13 13:42:04 -0300

    Documentation y improvements for tag stores

--- src/stores/memory-tag-store.ts
+++ src/stores/memory-tag-store.ts
@@ -11,11 +11,10 @@ export class MemoryTagStore implements TagStore {
 	}
 
 	async getMany(names: Iterable<string>): Promise<Tag[]> {
-		this.#cleanAllExpiredCache();
-
 		const result: Tag[] = [];
 
 		for (const name of names) {
+			this.#cleanIfExpired(name);
 			const tag = this.#cache.get(name);
 			if (tag) result.push(tag);
 		}
@@ -23,35 +22,20 @@ export class MemoryTagStore implements TagStore {
 		return result;
 	}
 
-	async getOne(name: string): Promise<Tag | null | undefined> {
+	async getOne(name: string): Promise<Tag | undefined> {
 		this.#cleanIfExpired(name);
 		return this.#cache.get(name);
 	}
 
 	async setMany(tags: Iterable<Tag>): Promise<void> {
-		for (const tag of tags) this.setOne(tag);
+		for (const tag of tags) this.#cache.set(tag.name, tag);
 	}
 
 	async setOne(tag: Tag): Promise<void> {
 		this.#cache.set(tag.name, tag);
 	}
 
-	/**
-	 * @description
-	 * Libera memoria de todas las Tags guardadas en caché que no se han refrescado en mucho tiempo.
-	 */
-	#cleanAllExpiredCache() {
-		const now = Date.now();
-
-		for (const [key, tag] of this.#cache.entries())
-			if (now - +tag.fetchTimestamp > MemoryTagStore.TAGS_CACHE_LIFETIME)
-				this.#cache.delete(key);
-	}
-
-	/**
-	 * @description
-	 * Libera memoria de Tags guardadas en caché que no se han refrescado en mucho tiempo.
-	 */
+	/**@description Cleans up a stored {@link Tag} if it hasn't been cached in a while.*/
 	#cleanIfExpired(name: string) {
 		const now = Date.now();
 		const tag = this.#cache.get(name);
--- src/stores/tag-store.ts
+++ src/stores/tag-store.ts
@@ -1,8 +1,29 @@
 import type { Tag } from '../models/tag';
 
 export default interface TagStore {
+	/**
+	 * @description Obtains multiple {@link Tag}s from this {@link TagStore} based on the provided `names`.
+	 * @param names The names to obtain tags from.
+	 * @returns An array of all the obtained tags. Returns an empty array if no tags were obtained.
+	 */
 	getMany(names: Iterable<string>): Promise<Tag[]>;
-	getOne(name: string): Promise<Tag | null | undefined>;
+
+	/**
+	 * @description Obtains a {@link Tag} from this {@link TagStore} based on the provided `name`.
+	 * @param name The names to obtain a tag from.
+	 * @returns The obtained tags, or `undefined` if none was obtained.
+	 */
+	getOne(name: string): Promise<Tag | undefined>;
+
+	/**
+	 * @description Stores multiple {@link Tag}s into this {@link TagStore}.
+	 * @param tags The tags to store.
+	 */
 	setMany(tags: Iterable<Tag>): Promise<void>;
+
+	/**
+	 * @description Stores a {@link Tag} into this {@link TagStore}.
+	 * @param tag The tag to store.
+	 */
 	setOne(tag: Tag): Promise<void>;
 }
