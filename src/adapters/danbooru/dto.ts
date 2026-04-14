7f6158ce6145f461fb6ffed06613d88d5db00742
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-14 02:28:42 -0300

    Add fields to DanbooruPostDto

--- src/adapters/danbooru/dto.ts
+++ src/adapters/danbooru/dto.ts
@@ -1,6 +1,10 @@
 export interface DanbooruPostDto {
 	id: number;
 	tag_string: string;
+	created_at: string;
+	large_file_url: string;
+	image_width: number;
+	image_height: number;
 }
 
 export type DanbooruPostsResponseDto = DanbooruPostDto[];
