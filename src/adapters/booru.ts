99e8fb868ae65de02f76d174250e0385006ca760
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-13 23:18:49 -0300

    Generalize Credentials in BooruClient
    Also delegate credentials validation to implementing Booru adapters.

--- src/adapters/booru.ts
+++ src/adapters/booru.ts
@@ -1,35 +1,41 @@
 import type { Post } from '../models/post';
 import type { Tag } from '../models/tag';
-import type { BooruSearchOptions, Credentials } from '../types/gelbooru';
+import type { BooruSearchOptions } from '../types/gelbooru';
 
 /**@description Defines una interface to interact with one of various imageboards.*/
-export default interface Booru {
+export default interface Booru<TCredentials = unknown> {
 	/**
 	 *
 	 * @param tags
 	 * @param options
 	 * @param credentials
 	 */
-	search(tags: string, searchOptions: Required<BooruSearchOptions>, credentials: Credentials): Promise<Post[]>;
+	search(tags: string, searchOptions: Required<BooruSearchOptions>, credentials: TCredentials): Promise<Post[]>;
 
 	/**
 	 *
 	 * @param postId
 	 * @param credentials
 	 */
-	fetchPostById(postId: string, credentials: Credentials): Promise<Post>;
+	fetchPostById(postId: string, credentials: TCredentials): Promise<Post>;
 
 	/**
 	 *
 	 * @param postUrl
 	 * @param credentials
 	 */
-	fetchPostByUrl(postUrl: URL | string, credentials: Credentials): Promise<Post>;
+	fetchPostByUrl(postUrl: URL | string, credentials: TCredentials): Promise<Post>;
 
 	/**
 	 *
 	 * @param names
 	 * @param credentials
 	 */
-	fetchTagsByNames(names: Iterable<string>, credentials: Credentials): Promise<Tag[]>;
+	fetchTagsByNames(names: Iterable<string>, credentials: TCredentials): Promise<Tag[]>;
+
+	/**
+	 * 
+	 * @param credentials 
+	 */
+	validateCredentials(credentials: TCredentials | undefined): asserts credentials is TCredentials;
 }
