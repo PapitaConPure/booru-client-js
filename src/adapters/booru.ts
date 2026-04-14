1d334fb26adc627e33200f2880e74cec1c0053d8
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-14 01:34:34 -0300

    format

--- src/adapters/booru.ts
+++ src/adapters/booru.ts
@@ -3,14 +3,21 @@ import type { Tag } from '../models/tag';
 import type { BooruSearchOptions } from '../types/booru';
 
 /**@description Defines una interface to interact with one of various imageboards.*/
-export default interface Booru<TCredentials = unknown, TSearchOptions extends BooruSearchOptions = BooruSearchOptions> {
+export default interface Booru<
+	TCredentials = unknown,
+	TSearchOptions extends BooruSearchOptions = BooruSearchOptions,
+> {
 	/**
 	 *
 	 * @param tags
 	 * @param options
 	 * @param credentials
 	 */
-	search(tags: string | string[], searchOptions: Required<TSearchOptions>, credentials: TCredentials): Promise<Post[]>;
+	search(
+		tags: string | string[],
+		searchOptions: Required<TSearchOptions>,
+		credentials: TCredentials,
+	): Promise<Post[]>;
 
 	/**
 	 *
@@ -34,8 +41,8 @@ export default interface Booru<TCredentials = unknown, TSearchOptions extends Bo
 	fetchTagsByNames(names: Iterable<string>, credentials: TCredentials): Promise<Tag[]>;
 
 	/**
-	 * 
-	 * @param credentials 
+	 *
+	 * @param credentials
 	 */
 	validateCredentials(credentials: TCredentials): asserts credentials is TCredentials;
 }
