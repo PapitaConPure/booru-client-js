b519fd04c80db4f4439a05e7a2068a18dbfb293e
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-14 01:56:26 -0300

    Define Gelbooru response DTOs

--- src/adapters/gelbooru/dto.ts
+++ src/adapters/gelbooru/dto.ts
@@ -1,34 +1,31 @@
-import type { ValuesOf } from '../../types/util';
-
-export interface GelbooruCredentials {
-	apiKey: string;
-	userId: string;
+export interface GelbooruPostsResponseDto {
+	post: {
+		id: number;
+		title: string;
+		tags: string[] | string;
+		source: string | string[];
+		score: number;
+		rating: string;
+		created_at: Date | string | number;
+		creator_id: number;
+		file_url: string;
+		width: number;
+		height: number;
+		preview_url?: string;
+		preview_width?: number;
+		preview_height?: number;
+		sample_url?: string;
+		sample_width?: number;
+		sample_height?: number;
+	}[];
 }
 
-export const GelbooruPostRatings = {
-	General: 'general',
-	Sensitive: 'sensitive',
-	Questionable: 'questionable',
-	Explicit: 'explicit',
-} as const;
-export type PostRating = ValuesOf<typeof GelbooruPostRatings>;
-
-export interface GelbooruAPIPostData {
-	id: number;
-	title: string;
-	tags: string[] | string;
-	source: string | string[];
-	score: number;
-	rating: PostRating;
-	created_at: Date | string | number;
-	creator_id: number;
-	file_url: string;
-	width: number;
-	height: number;
-	preview_url?: string;
-	preview_width?: number;
-	preview_height?: number;
-	sample_url?: string;
-	sample_width?: number;
-	sample_height?: number;
+export interface GelbooruTagsResponseDto {
+	tag: {
+		id: number;
+		name: string;
+		count?: number;
+		type?: number;
+		ambiguous?: boolean;
+	}[];
 }
