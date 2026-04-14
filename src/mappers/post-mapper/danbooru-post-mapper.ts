9c4258683cb95eb6d9d05daba4b02595e68fd802
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-14 06:09:52 -0300

    Fix Danbooru PostMapper implementation

--- src/mappers/post-mapper/danbooru-post-mapper.ts
+++ src/mappers/post-mapper/danbooru-post-mapper.ts
@@ -1,32 +1,40 @@
-import type { DanbooruPostDto } from '../../adapters/danbooru/dto';
-import type { GelbooruPostRating } from '../../adapters/gelbooru/dto';
+import type { DanbooruPostDto, DanbooruPostRating } from '../../adapters/danbooru/dto';
 import { Post } from '../../domain/post';
 import { type PostRating, PostRatings } from '../../domain/post-rating';
 import type { PostMapper } from '../post-mapper';
 
 const danbooruRatingsMap = {
-	general: PostRatings.General,
-	sensitive: PostRatings.Sensitive,
-	questionable: PostRatings.Questionable,
-	explicit: PostRatings.Explicit,
-} as const satisfies Record<GelbooruPostRating, PostRating>;
+	g: PostRatings.General,
+	s: PostRatings.Sensitive,
+	q: PostRatings.Questionable,
+	e: PostRatings.Explicit,
+} as const satisfies Record<DanbooruPostRating, PostRating>;
 
 export class DanbooruPostMapper implements PostMapper<DanbooruPostDto> {
 	fromDto(dto: DanbooruPostDto): Post {
+		const sources = dto.source
+			.split(/\s+/)
+			.map((s) => s.trim())
+			.filter((s) => s != null);
+
 		return new Post({
 			id: dto.id,
 			title: '',
 			tags: dto.tag_string.split(' '),
-			sources: undefined,
-			score: -1,
-			rating: danbooruRatingsMap.general,
+			sources: sources.length ? sources : undefined,
+			score: dto.score,
+			rating: danbooruRatingsMap[dto.rating],
 			createdAt: new Date(dto.created_at),
-			creatorId: 0,
-			fileUrl: dto.large_file_url,
+			creatorId: dto.uploader_id,
+			fileUrl:
+				(dto.has_large ? dto.large_file_url : dto.file_url)
+				|| dto.file_url
+				|| dto.preview_file_url
+				|| '',
 			size: [dto.image_width, dto.image_height],
-			previewUrl: undefined,
+			previewUrl: dto.preview_file_url,
 			previewSize: undefined,
-			sampleUrl: undefined,
+			sampleUrl: dto.has_large ? dto.file_url : undefined,
 			sampleSize: undefined,
 		});
 	}
