6aa314d7c29143a84b91917e8974c6f8fc030e36
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-13 22:23:02 -0300

    Support mocking both post and tag API endpoints

--- src/tests/mocks/fetchExt.test.ts
+++ src/tests/mocks/fetchExt.test.ts
@@ -1,13 +1,31 @@
 import { mock } from 'bun:test';
 
 mock.module('../../utils/fetchExt', () => ({
-	fetchExt: async () => ({
-		success: true,
-		data: {
-			tag: [
-				{ id: 1, name: 'kishin_sagume' },
-				{ id: 2, name: 'junko_(touhou)' },
-			],
-		},
-	}),
+	fetchExt: async (params: URL) => {
+		let data: unknown;
+
+		console.log(params);
+
+		switch (params.searchParams.get('s')) {
+			case 'post':
+				data = {
+					post: [{ id: 1, tags: 'clownpiece' }],
+				};
+				break;
+
+			case 'tag':
+				data = {
+					tag: [
+						{ id: 1, name: 'kishin_sagume' },
+						{ id: 2, name: 'junko_(touhou)' },
+					],
+				};
+				break;
+		}
+
+		return {
+			success: true,
+			data,
+		};
+	},
 }));
