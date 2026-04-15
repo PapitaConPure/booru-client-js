38562d667eb2bac51d0d9379a6139d4b4b7f5a97
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-15 11:49:33 -0300

    Add Mapper interface documentation

--- src/mappers/post-mapper.ts
+++ src/mappers/post-mapper.ts
@@ -1,6 +1,7 @@
 import type { Booru } from '../adapters/booru';
 import type { Post } from '../domain/post';
 
+/**Defines an interface for mapping DTOs to {@link Post}s which are associated to a {@link Booru}.*/
 export interface PostMapper<TDto, TBooru extends Booru> {
 	fromDto(dto: TDto): Post<TBooru>;
 }
--- src/mappers/tag-mapper.ts
+++ src/mappers/tag-mapper.ts
@@ -1,5 +1,6 @@
 import type { Tag } from '../domain/tag';
 
+/**Defines an interface for mapping DTOs to {@link Tag}s.*/
 export interface TagMapper<TDto> {
 	fromDto(dto: TDto): Tag;
 }
