aa833d4d93c099ed8008c5952dacaa10967448fc
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-20 10:52:10 -0300

    Define schemas of Konachan Post DTOs

---
+++ src/adapters/konachan/dto.ts
@@ -0,0 +1,44 @@
+export type KonachanPostRating = 's' | 'q' | 'e';
+
+export type KonachanPostStatus = 'active' | 'pending' | 'deleted' | 'flagged';
+
+export interface KonachanPostDto {
+	id: number;
+	tags: string;
+	created_at: number;
+	creator_id: number;
+	author: string;
+	change: number;
+	source: string;
+	score: number;
+	md5: string;
+	file_size: number;
+	file_url: string;
+	is_shown_in_index: boolean;
+	preview_url: string;
+	preview_width: number;
+	preview_height: number;
+	actual_preview_width: number;
+	actual_preview_height: number;
+	sample_url: string;
+	sample_width: number;
+	sample_height: number;
+	sample_file_size: number;
+	jpeg_url: string;
+	jpeg_width: number;
+	jpeg_height: number;
+	jpeg_file_size: number;
+	rating: KonachanPostRating;
+	has_children: boolean;
+	parent_id: string | null;
+	status: KonachanPostStatus;
+	width: number;
+	height: number;
+	is_held: boolean;
+	frames_pending_string: string;
+	frames_pending: string[];
+	frames_string: string;
+	frames: string[];
+}
+
+export type KonachanPostsResponseDto = KonachanPostDto[];
