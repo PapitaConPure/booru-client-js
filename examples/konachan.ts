87a1c84fe5c5278c180b1bdd8ebaac9ba67783db
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-20 13:05:22 -0300

    Update Konachan example

--- examples/konachan.ts
+++ examples/konachan.ts
@@ -4,21 +4,19 @@ import { BooruClient, Konachan } from '@papitaconpure/booru-client';
 const konachan = new Konachan();
 
 //Create a Booru client with the adapter and our credentials
-const client = new BooruClient(konachan, {
-	credentials: {},
-	tags: {
-
-	},
-});
+const client = new BooruClient(konachan, {});
 
 //Return the last 5 posts containing the tag "touhou" and a "Safe" content rating
-const posts = await client.search('hololive', { limit: 5, rating: 's' });
+const posts = await client.search('hololive', { limit: 1, random: true });
 
 //Log the id, tags and url of every obtained post
 for (const post of posts) {
-	console.log({
-		id: post.id,
-		tags: post.tags,
-		url: post.url,
-	});
+	console.dir(
+		{
+			id: post.id,
+			tags: (await client.fetchPostTags(post)).map((t) => `${t.name} (${t.count})`),
+			url: post.url,
+		},
+		{ depth: null },
+	);
 }
