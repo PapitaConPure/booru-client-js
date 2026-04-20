1753b8233607df6d2f1bf15a86dbc9d51790f3b1
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-20 10:51:23 -0300

    Add Konachan types (tentative?)

--- src/adapters/konachan/types.ts
+++ src/adapters/konachan/types.ts
@@ -1,4 +1,18 @@
-export interface KonachanSearchOptions {
+import type { PostMapper } from '../../mappers/post-mapper';
+import type { DanbooruOptions, DanbooruPostExtra, DanbooruSearchOptions } from '../danbooru/types';
+import type { Konachan } from './client';
+import type { KonachanPostDto } from './dto';
+
+export interface KonachanOptions extends Omit<DanbooruOptions, 'postMapper'> {
+	/**Mapper used to transform Konachan post DTOs into {@link Post} domain entities.*/
+	postMapper?: PostMapper<KonachanPostDto, Konachan>;
+}
+
+export interface KonachanCredentials extends Record<string, never> {}
+
+export interface KonachanSearchOptions extends DanbooruSearchOptions {
 	width?: number;
 	height?: number;
 }
+
+export interface KonachanPostExtra extends DanbooruPostExtra {}
