7a74ec628ed734560908e381fc35cff818b2ec96
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2026-04-20 13:07:20 -0300

    Add Konachan Tag Mapper (basically Gelbooru)

---
+++ src/mappers/tag-mapper/konachan-tag-mapper.ts
@@ -0,0 +1,27 @@
+import {
+	type KonachanTagDto,
+	type KonachanTagType,
+	KonachanTagTypes,
+} from '../../adapters/konachan/dto';
+import { Tag } from '../../domain/tag';
+import { type TagType, TagTypes } from '../../domain/tag-type';
+import type { TagMapper } from '../tag-mapper';
+
+const gelbooruTagTypesMap = {
+	[KonachanTagTypes.GENERAL]: TagTypes.GENERAL,
+	[KonachanTagTypes.ARTIST]: TagTypes.ARTIST,
+	[KonachanTagTypes.COPYRIGHT]: TagTypes.COPYRIGHT,
+	[KonachanTagTypes.CHARACTER]: TagTypes.CHARACTER,
+	[KonachanTagTypes.METADATA]: TagTypes.METADATA,
+} as const satisfies Record<KonachanTagType, TagType>;
+
+export class KonachanTagMapper implements TagMapper<KonachanTagDto> {
+	fromDto(dto: KonachanTagDto): Tag {
+		return new Tag({
+			id: `${dto.id}`,
+			name: dto.name,
+			type: gelbooruTagTypesMap[dto.type],
+			count: dto.count,
+		});
+	}
+}
